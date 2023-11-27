073e6959a4270bcd8bfb063b396b8414ebce35c0
danilofernandes45 <danilofernandes45@gmail.com>
2023-11-27 13:54:57 -0300

    Addicional features

--- plot_plan.py
+++ plot_plan.py
@@ -46,7 +46,7 @@ def process_bandt_pomp():
         image_read = cv2.imread(file)
         # conversion numpy array into rgb image to show
         img = cv2.cvtColor(image_read, cv2.COLOR_BGR2GRAY)
-        ord_dis = ordpy.ordinal_distribution(img, dx = 2, dy = 2, taux = 1, tauy = 1)
+        ord_dis = ordpy.ordinal_distribution(img, dx = 3, dy = 3, taux = 2, tauy = 2)
         data.append([img_id] + list(ord_dis[1]))
 
     data = pd.DataFrame(data).sort_values(0).reset_index(drop = True)
@@ -59,18 +59,46 @@ def process_bandt_pomp():
                   "normal": "Healthy",
                   "vidro_fosco": "Ground-glass Opacity"}
     data.Label = data.Label.apply(lambda x : dict_label[x] if x in dict_label.keys() else x)
-    data.to_csv("BP_data.csv", index = False)
+    data.to_csv("BP_data_3_2.csv", index = False)
+    return data
+
+def process_smoothness_structure():
+    data = []
+    folder = os.getcwd() + "/data/images/*.*"
+    for file in glob.glob(folder):
+        img_id = int(file.split("/")[-1].split(".")[0])
+        image_read = cv2.imread(file)
+        # conversion numpy array into rgb image to show
+        img = cv2.cvtColor(image_read, cv2.COLOR_BGR2GRAY)
+        ord_dis = ordpy.smoothness_structure(img)
+        data.append([img_id, ord_dis[0], ord_dis[1]])
+
+    data = pd.DataFrame(data).sort_values(0).reset_index(drop = True)
+    labels = pd.read_csv("data/labels/groups_DPD.txt", header = None)
+    data["Label"] = labels
+    dict_label = {"consolidacao": "Pulmonary Consolidation",
+                  "enfisema": "Emphysematous Area",
+                  "espessamento": "Septal Thickening",
+                  "favo_de_mel": "Honeycomb",
+                  "normal": "Healthy",
+                  "vidro_fosco": "Ground-glass Opacity"}
+    data.Label = data.Label.apply(lambda x : dict_label[x] if x in dict_label.keys() else x)
+    data.to_csv("SS_data.csv", index = False)
     return data
 
 # data_hc, data_fs = process_data()
 
 process_bandt_pomp()
+# process_smoothness_structure()
 
-data_hc = pd.read_csv("HC_data.csv")
-data_fs = pd.read_csv("FS_data.csv")
+data_ss = pd.read_csv("SS_data.csv")
+# data_hc = pd.read_csv("HC_data.csv")
+# data_fs = pd.read_csv("FS_data.csv")
 
 plt.rcParams.update({"font.size": 17})
-sns.jointplot(data_hc, x = 'Entropy', y = "Complexity", hue = 'Label', s = 70)
-plt.show()
-sns.jointplot(data_fs, x = 'Entropy', y = "Fisher information", hue = 'Label', s = 70)
+# sns.jointplot(data_hc, x = 'Entropy', y = "Complexity", hue = 'Label', s = 70)
+# plt.show()
+# sns.jointplot(data_fs, x = 'Entropy', y = "Fisher information", hue = 'Label', s = 70)
+# plt.show()
+sns.jointplot(data_ss, x = 'Smoothness', y = "Curve structure", hue = 'Label', s = 70)
 plt.show()
